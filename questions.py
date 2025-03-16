4c0334bb86984ebedd2fbfaa6e138dee96500820
pablolube <pablolube@gmail.com>
2025-03-16 17:38:50 -0300

    2-Agregue calculo de puntaje

--- questions.py
+++ questions.py
@@ -1,6 +1,6 @@
 import random
 import sys
-import random
+
 
 # Preguntas para el juego
 questions = [
@@ -27,7 +27,10 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
+puntaje=0
+
 for _ in range(3):
+
     # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
 
@@ -40,6 +43,8 @@ for _ in range(3):
 # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         user_answer = (input("Respuesta: ")) 
+# Verifica si el dato ingresado por el usuario es un dato dentro del rango de opciones de respuesta
+
         if  not user_answer.isdigit() or (int(user_answer) - 1) not in range(0,len(answers[question_index])):
             print("Respuesta no válida")
             sys.exit(1)
@@ -49,12 +54,18 @@ for _ in range(3):
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntaje+=1
             break
     else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
+        
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
-
+        puntaje-=1 
+        print()
+ 
     # Se imprime un blanco al final de la pregunta
-    print()
+print(f"El puntaje total es")
+print(puntaje)
+print("")
