836ecda2f9ebdc1818ceac217bc76d09013edf33
pablolube <pablolube@gmail.com>
2025-03-21 21:45:47 -0300

    TP 2 - Resolucion ejercicio 1

--- Actividad_2/src/__init__.py
+++ Actividad_2/src/__init__.py
@@ -1,14 +1,11 @@
 
 #Funcion para filtrar lineas cuyas segunda palabra inicia con una vocal
-def vowel_filter (text):
-    vowels=('AEIOUaeiou')
-    
-    lines = text.split('\n')
-    
-    for line in lines:
-        word=line.split()
-        if len(word) > 1:
-            second_word=word[1]
-            if second_word[0] in(vowels):
+def filtrar_2dapalabra_vocal(text):
+    vocal=("AEIOUaeiou")
+    lines= text.split('\n')
+    for line in lines: 
+        if len(line)>2:
+            word=line.split(' ')
+            if word[1][0] in vocal:
                 print(line)
 
