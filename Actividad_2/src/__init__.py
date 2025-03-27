91decf80d08cc459e688d136f4c76a7d01901243
pablolube <pablolube@gmail.com>
2025-03-26 23:04:48 -0300

    TP2 - Incorpore reordenamiento por puntaje punto 10

--- Actividad_2/src/__init__.py
+++ Actividad_2/src/__init__.py
@@ -69,9 +69,11 @@ def validacion_usuario(usuario):
 #5 Funcion de velocidad de reacción en un juego
 
 def tiempo_reaccion(tiempo):
+        if not tiempo.isdigit():
+             print("No ha ingresado un numero, ingrese la veolicidad de reaccion en ms")
         tiempo=int(tiempo)
          
-        if  tiempo <0:
+        if  tiempo <0 :
             print("Tiempo ingresado es menor a 0, vuelva a ingresar")
             return
         reaccion="" 
@@ -167,3 +169,76 @@ def limpieza_nombres(nombres):
         nombres=elimino_repetidos(nombres)
         return nombres
 #10  Limpieza de Datos para un Sistema de Clientes
+def inicializo(tabla_general,jugador):
+    if jugador not in tabla_general:
+        tabla_general[jugador]={'kills': 0, 'assists': 0, 'deaths': 0,'MVPs':0,'Puntajes':0}
+    return tabla_general
+
+
+def calculo_puntaje(ronda_datos, jugador, puntos):
+    ronda_datos[jugador]['Puntajes'] = (ronda_datos[jugador]['kills'] * puntos['kills'] +
+                                        ronda_datos[jugador]['assists'] * puntos['assists'] +
+                                        int(ronda_datos[jugador]['deaths']) * puntos['deaths'])
+    return ronda_datos[jugador]
+
+
+def calcula_max_ronda(ronda_datos,jugador,max_ronda,jugador_mvp):
+    if ronda_datos[jugador]['Puntajes']>max_ronda:
+       max_ronda=ronda_datos[jugador]['Puntajes']
+       jugador_mvp=jugador
+    return jugador_mvp,max_ronda
+
+def acumulo_ronda(tabla_general, jugador, rdo_jugador_ronda):
+    tabla_general[jugador]['kills'] += rdo_jugador_ronda[jugador]['kills']
+    tabla_general[jugador]['assists'] += rdo_jugador_ronda[jugador]['assists']
+    tabla_general[jugador]['deaths'] += int(rdo_jugador_ronda[jugador]['deaths'])
+    tabla_general[jugador]['Puntajes'] += rdo_jugador_ronda[jugador]['Puntajes']
+    return tabla_general
+
+#PROCESO RONDA
+def procesar_ronda(ronda_datos, tabla_general,puntos):
+    max_ronda=0
+    jugador_mvp=""
+    for jugador in  ronda_datos:
+
+        #Inicializo la tabla general si el jugador es nuevo
+        inicializo(tabla_general,jugador)
+
+        #Calculo puntaje 
+        calculo_puntaje(ronda_datos,jugador,puntos)
+
+        #Guarda si es el max puntaje ronda       
+        jugador_mvp,max_ronda=calcula_max_ronda(ronda_datos,jugador,max_ronda,jugador_mvp)
+        
+
+        #Acumulo resultados de ronda a la tabla géneral
+        acumulo_ronda(tabla_general,jugador,ronda_datos)
+        
+        #inicializo mvp de la ronda
+        ronda_datos[jugador]['MVPs'] = 0
+             
+       
+    if jugador_mvp:
+        ronda_datos[jugador_mvp]['MVPs'] = 1 
+        tabla_general[jugador_mvp]['MVPs'] += 1
+    
+    
+    return tabla_general
+
+def imprimir_tabla(tabla):
+    print("{:<10} {:<7} {:<7} {:<7} {:<7} {:<10}".format("Jugador", "Kills", "Asists", "Deaths", "MVPs", "Puntajes"))
+    print("-" * 50)
+    
+    tabla_ordenada = sorted(tabla.items(), key=lambda x: x[1]['Puntajes'], reverse=True)
+
+    for jugador, stats in tabla_ordenada:
+        print("{:<10} {:<7} {:<7} {:<7} {:<7} {:<10}".format(
+            jugador,
+            stats['kills'],
+            stats['assists'],
+            stats['deaths'],
+            stats['MVPs'],
+            stats['Puntajes']
+        ))
+    print("\n")
+    
